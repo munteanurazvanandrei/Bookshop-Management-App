36bc86d25dc30fd592b3848d2c794baef788191c
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-20 10:08:30 +0300

    BMA-24 Created dashboard components

---
+++ client/src/Components/Employee/Dashboard/EmployeedDashboard.js
@@ -0,0 +1,4 @@
+export default function EmployeeDashboard(){
+    // Employee dashboard
+    return(<></>)
+}
---
+++ client/src/Components/Manager/Dashboard/ManagerDashboard.js
@@ -0,0 +1,4 @@
+export default function ManagerDashboard(){
+    // Manager Dashboard
+    return(<></>)
+}
