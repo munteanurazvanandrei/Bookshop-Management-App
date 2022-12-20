62031bf3cb38f211f3a91ac8253647dd43e98ab3
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-20 10:02:02 +0300

    BMA-24 added a route for the landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import {LandingPage} from './Components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="*" element={<LandingPage/>}/>
+      </Routes>
     </div>
   );
 }
