6e013ad7867b0736660735dd53bbbdefc9f4b995
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-21 22:15:36 +0300

    BMA-25 adds controller actions for the manager controller

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -1,13 +1,31 @@
 class ManagersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
     before_action :authorized
     # GET /managers
     def index
         render json: Manager.all
     end
 
-    # POST /managers
-    def create
-        manager = Manager.create!(manager_params)
-        if manager.valid?
+    # GET /managers/:id
+    def show
+        render json: manager, status: :ok
+    end
+
+    # DELETE /managers/:id
+    def destroy
+        manager = find_manager
+        manager.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_manager
+        Manager.find_by!(id: params[:id])
+    end
+
+    def record_not_found
+        render json: { error: "Manager not found" }, status: :no_found        
     end
 end
