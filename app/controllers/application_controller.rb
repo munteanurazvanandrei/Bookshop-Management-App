da7e817caf4f5ac04b4e930480f5822aa78a4b10
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-21 14:18:24 +0300

    BMA-25 adds JWT encode and decode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,43 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_method
+
+    def encode_token
+        JWT.encode(payload, 'pass_phrase')
+    end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'pass_phrase', true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
+
+    def current_user
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            @user = User.find_by(id: user_id)
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def authorized
+        render json: { message: 'Please sign in' }, status: :unauthorized unless logged_in?
+    end
+
+    private
+
+    def unprocessable_entity_method entity
+        render json: { errors: entity.record.errors.full_messages },status: :unprocessable_entity
+    end
 end
