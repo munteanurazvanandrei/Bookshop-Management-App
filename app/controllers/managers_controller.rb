39e57f2459e9b1ce7269223d0059530eada995a9
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-22 18:53:26 +0300

    BMA-26 added an update to managers_controller

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -1,6 +1,7 @@
 class ManagersController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     before_action :authorized, except: [:create]
+    before_action :is_manager?, except: [:index]
     # GET /managers
     def index
         render json: Manager.all
@@ -22,6 +23,13 @@ rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
         manager = find_manager
         manager.destroy
         head :no_content
+    end
+
+    #PATCH /managers/:id
+    def update
+        manager = find_manager
+        manager.update!(manager_params)
+        render json: manager, status: :accepted
     end
 
     private
