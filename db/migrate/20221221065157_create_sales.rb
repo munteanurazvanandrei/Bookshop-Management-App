1f01474ffa709d16ca28a448fd3d59ffd38e82e3
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-22 21:27:04 +0300

    BMA-26 sales migration-update : sales_transactions

--- db/migrate/20221221065157_create_sales.rb
+++ db/migrate/20221221065157_create_sales.rb
@@ -1,8 +1,7 @@
 class CreateSales < ActiveRecord::Migration[7.0]
   def change
     create_table :sales do |t|
-      t.integer :transaction_id
-      t.string :item_id_integer
+      t.integer :sales_transaction_id
       t.integer :item_id
       t.integer :item_price_at_sale, default: 0
       t.integer :qty, default: 0
