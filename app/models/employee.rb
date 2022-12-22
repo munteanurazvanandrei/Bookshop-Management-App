50bc9efd36bebd43fe86093cb6aa95ee06593019
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-22 18:54:08 +0300

    BMA-26 edited employee model relationships

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ApplicationRecord
     has_secure_password
     belongs_to :manager
-    has_many :sales
-    has_many :transactions, through: :sales
+    has_many :transactions
+    has_many :sales, through: :transactions
     validates :email, uniqueness: true
 end
