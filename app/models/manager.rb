0717e6776fac3c735bd19ad379d87a6ddc4eadd9
James Mburu Githire <jamesmburug24@gmail.com>
2022-12-22 11:22:34 +0300

    BMA-26 added validations to manager model

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -4,4 +4,7 @@ class Manager < ApplicationRecord
     has_many :employees
     has_many :sales, through: :employees
     has_many :transactions, through: :sales
+    validates :email, presence: true, uniqueness: true
+    validates :bookshop_name, presence: true
+    validates :bookshop_items_alert_limit, presence: true
 end
