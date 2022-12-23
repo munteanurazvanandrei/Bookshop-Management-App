558f4b90ed7c21f02eb7d8a154a1ef8ca926ffcb
Gibson-Bett <gibubett@gmail.com>
2022-12-23 09:57:28 +0300

    seed data to managers tables.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Manager.create(name: "Karl", email: "emailKarl@email.com", password: "Karl")
-Item.create(manager_id: 1, name_or_title: "That cool book", manufacturer_or_author: "John", price_per_item:100, qty: 50)
+
+puts "Seeding begins here..."
+
+10.times do
+    Manager.create(name:Faker::Name.name, email:Faker::Internet.email, password_digest:Faker::Internet.password, bookshop_name:Faker::Company.name, bookshop_items_alert_limit:100  )
+end
+# Manager.create(name: "Karl", email: "emailKarl@email.com", password: "Karl")
+
+Item.create([
+    manager_id: 1, name_or_title: "That cool book", manufacturer_or_author: "John", price_per_item:100, qty: 50
+    
+    ])
+
 Employee.create(manager_id: 1, email: "employee201@email.com", password: "asd")
+
 SalesTransaction.create(amount: 1000, recieved: 1000,change:0, employee_id:1)
+
 Sale.create( sales_transaction_id: 1, item_id:1, item_price_at_sale: 500, qty: 2)
 # 
-Manager.create(name: "Xarl", email: "emailXarl@email.com", password: "Xarl")
-Item.create(manager_id: 2, name_or_title: "That cool book", manufacturer_or_author: "John", price_per_item:100, qty: 50)
-Employee.create(manager_id: 2, email: "xemployee201@email.com", password: "asd")
-SalesTransaction.create(amount: 1000, recieved: 1000,change:0, employee_id:2)
-Sale.create( sales_transaction_id: 2, item_id:1, item_price_at_sale: 200, qty: 2)
+# Manager.create(name: "Xarl", email: "emailXarl@email.com", password: "Xarl")
+# Item.create(manager_id: 2, name_or_title: "That cool book", manufacturer_or_author: "John", price_per_item:100, qty: 50)
+# Employee.create(manager_id: 2, email: "xemployee201@email.com", password: "asd")
+# SalesTransaction.create(amount: 1000, recieved: 1000,change:0, employee_id:2)
+# Sale.create( sales_transaction_id: 2, item_id:1, item_price_at_sale: 200, qty: 2)
+
+puts "... Seeding ends here"
