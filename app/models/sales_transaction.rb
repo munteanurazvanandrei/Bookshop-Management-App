9ba22e7730b5ab1c574ef5be8104367ed9e0a86a
Gibson-Bett <gibubett@gmail.com>
2022-12-23 15:47:06 +0300

    comment out validation to generate seeds

--- app/models/sales_transaction.rb
+++ app/models/sales_transaction.rb
@@ -2,5 +2,5 @@ class SalesTransaction < ApplicationRecord
     has_many :sales
     belongs_to :employee
     delegate :manager, to: :employee 
-    validates :employee_id, presence: true
+    # validates :employee_id, presence: true
 end
