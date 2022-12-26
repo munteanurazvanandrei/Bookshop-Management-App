6c7e3198e9e7a30246b12b0d3d2ecf14892a0308
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-26 08:26:31 +0300

    BMA-25 adds Sale component

--- client/src/Components/Employee/MakeASale/MakeASale.js
+++ client/src/Components/Employee/MakeASale/MakeASale.js
@@ -1,4 +1,42 @@
-export default function MakeASale(){
-    // Point of sale
-    return(<div className="make-a-sale"></div>);
+import { NavLink, useNavigate } from 'react-router-dom'
+import '../../dist/sale.css'
+
+export default function MakeASale({ employeeName }) {
+    // point of sale navigation pane
+
+    // Redirects user to set-page e.g. home, login
+    const nav = useNavigate()
+    return (
+        <div className='point-of-sale'>
+            <div className="pos-sidebar">
+                <h3>Make a Sale</h3>
+                <img src='/svgs/employees.svg' alt='employee' />
+                <p>{employeeName}</p>
+                <hr />
+                <NavLink to="/dashboard">
+                    <button className='input-btn' type="submit">
+                        Back to Dashboard
+                    </button>
+                </NavLink>
+                <NavLink to="/cancel">
+                    <button className='input-btn' type="submit">
+                        Cancel
+                    </button>
+                </NavLink>
+                <NavLink to="/complete">
+                    <button className='input-btn' type="submit">
+                        Complete
+                    </button>
+                </NavLink>
+                {/* Footer */}
+                <div className='bottom'>
+                    <hr />
+                    <div onClick={/*Logout and navigate to landing page */ () => { nav("/") }}>
+                        <img src='/svgs/logout.svg' alt="logout" />
+                        <span>Logout</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
