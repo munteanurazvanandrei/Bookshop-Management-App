2db72beadb647de7025267af62a772ce7d4063ca
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-26 22:21:14 +0300

    BMA-25 byebug test

--- app/controllers/jwt_controller.rb
+++ app/controllers/jwt_controller.rb
@@ -4,6 +4,7 @@ class JwtController < ApplicationController
     
     # create action for user creation and auth
     def create
+        # byebug
         user = manager_access? ? 
             Manager.find_by!(email: params[:email]) : 
             Employee.find_by!(email: params[:email])
