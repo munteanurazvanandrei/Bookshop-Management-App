68c14a2520f30af1cab5dbf26d1acf1d784a6e7a
Gibson-Bett <gibubett@gmail.com>
2022-12-27 15:26:12 +0300

    BMA-16 # set state on form change

--- client/src/Components/SignInPage/RegistrationPage.js
+++ client/src/Components/SignInPage/RegistrationPage.js
@@ -1,5 +1,17 @@
+import React, { useState } from "react";
+
 export default function Registration(){
     // the registration for the manager;
     // also for the bookshop
+
+    const [formState, setFormState] = useState({});
+
+    function formChange(e) {
+		const value = e.target.value;
+		const name = e.target.name;
+		setFormState({ ...formState, [name]: value });
+	}
+
+
     return(<div className="registration"></div>)
 }
