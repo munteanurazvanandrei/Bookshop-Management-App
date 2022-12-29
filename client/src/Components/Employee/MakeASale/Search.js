9432df62f064496a144de457920f1302f5cef8a0
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-29 16:04:51 +0300

    BMA-18 adds search component functionality

--- client/src/Components/Employee/MakeASale/Search.js
+++ client/src/Components/Employee/MakeASale/Search.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Search = () => {
-  return (
-    <div>Search</div>
+    
+    // initialize state for user input during search
+    const [searchInput, setSearchInput] = useState("");
+
+    // function to handle any change while user is searching
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value)
+    }
+
+    // checks if search input field recieves any character and filters to match name of entity entered
+    (searchInput.length > 0) ? items.filter((item)=> item.name.match(searchInput)) : null
+
+    return (
+    <div>
+        {/* Returns search as an input type */}
+        <input type="search" placeholder='Search here' onChange={handleChange} value={searchInput} />
+    </div>
   )
 }
 
-export default Search
+export default Search;
