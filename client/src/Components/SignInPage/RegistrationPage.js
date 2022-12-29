664462091ff4cb3268bce15d3111e944512567ac
Gibson-Bett <gibubett@gmail.com>
2022-12-29 16:38:54 +0300

    BMA-16# return the signup form and navigate to login on successful signup

--- client/src/Components/SignInPage/RegistrationPage.js
+++ client/src/Components/SignInPage/RegistrationPage.js
@@ -1,33 +1,84 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Registration(){
-    // the registration for the manager;
-    // also for the bookshop
+export default function Registration() {
+  // the registration for the manager;
+  // also for the bookshop
 
-    const [formState, setFormState] = useState({});
+  const [formState, setFormState] = useState({});
+  const navigate = useNavigate();
 
-    function formChange(e) {
-		const value = e.target.value;
-		const name = e.target.name;
-		setFormState({ ...formState, [name]: value });
-	}
+  function formChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+    setFormState({ ...formState, [name]: value });
+  }
 
-    function handleSubmit(e){
-        e.preventDefault()
-        fetch("",{
-            method: "POST",
-            headers: {
-                "Content-type": "application/json",
-            },
-            body:JSON.stringify({
-                name: formState.name,
-                email: formState.email,
-                password: formState.password,
-                password_confirmation: formState.password_confirmation
-            }),
-        })
-        .then((r) => r.json())
-        .then((data) => data)
-    }
-    return(<div className="registration"></div>)
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: formState.name,
+        email: formState.email,
+        password: formState.password,
+        password_confirmation: formState.password_confirmation,
+      }),
+    })
+      .then((r) => r.json())
+      .then((data) => data);
+  }
+  return (
+    <div className="registration">
+      <div className="manager-login-form">
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <div className="login-inputs-container">
+            <input
+              id="name"
+              type="text"
+              name="name"
+              placeholder="Name"
+              // value={formState.name}
+              onChange={(e) => formChange(e)}
+              required
+            />
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Email address"
+              // value={formState.email}
+              onChange={(e) => formChange(e)}
+              required
+            />
+            <input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              // value={formState.password}
+              onChange={(e) => formChange(e)}
+              required
+            />
+            <input
+              id="passwordConfirm"
+              type="password"
+              name="password_confirmation"
+              placeholder="Confirm password"
+              // value={passConfirmation.passwordConfirm}
+              onChange={(e) => formChange(e)}
+              required
+            />
+          </div>
+
+          <div className="logins-button-form">
+            <button onClick={() => navigate("/login")}>Signup</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
