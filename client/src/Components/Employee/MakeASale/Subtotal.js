3184c5e0e4286acb48ccd7c947b06a0b62178cc2
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-12-29 16:05:34 +0300

    BMA-18 scafold subtotal component for test conduct

---
+++ client/src/Components/Employee/MakeASale/Subtotal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+// TODO: create subtotal states and return data from makeASale componennt
+const Subtotal = () => {
+  return (
+    <div>Subtotal</div>
+  )
+}
+
+export default Subtotal
