e7d776743b8ee5182d97e921dded6182f7c31a46
Gibson-Bett <gibubett@gmail.com>
2023-01-02 23:34:43 +0300

    BMA-16# import css file, import image file, add home button, breaks, and also the welcome message.

--- client/src/Components/SignInPage/RegistrationPage.js
+++ client/src/Components/SignInPage/RegistrationPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-
+import "./registration.css"
+import logo from "./download.jpeg"
 export default function Registration() {
   // the registration for the manager;
   // also for the bookshop
@@ -8,7 +9,7 @@ export default function Registration() {
   const [formState, setFormState] = useState({});
   const navigate = useNavigate();
   const myRouteLocation = window.location.pathname;
-
+  
   function formChange(e) {
     const value = e.target.value;
     const name = e.target.name;
@@ -33,67 +34,85 @@ export default function Registration() {
       .then((data) => data);
   }
   return (
-    <div className="registration">
-      <h2>PIONEER BOOKSHOP MANAGERS</h2>
-      <h3>Join Us</h3>
-      <div className="manager-login-form">
-        <form onSubmit={handleSubmit} autoComplete="off">
-          <div className="login-inputs-container">
-            <input
-              id="name"
-              type="text"
-              name="name"
-              placeholder="Name"
-              // value={formState.name}
-              onChange={(e) => formChange(e)}
-              required
-            />
-            <input
-              id="email"
-              type="email"
-              name="email"
-              placeholder="Email address"
-              // value={formState.email}
-              onChange={(e) => formChange(e)}
-              required
-            />
-            <input
-              id="password"
-              type="password"
-              name="password"
-              placeholder="Password"
-              // value={formState.password}
-              onChange={(e) => formChange(e)}
-              required
-            />
-            <input
-              id="passwordConfirm"
-              type="password"
-              name="password_confirmation"
-              placeholder="Confirm password"
-              // value={passConfirmation.passwordConfirm}
-              onChange={(e) => formChange(e)}
-              required
-            />
-          </div>
 
-          <div className="logins-button-form">
-            <button onClick={() => navigate("/login")}>Continue</button>
-          </div>
-        </form>
+    <div className="registration">
+      <div className="registration-nav">
+        <h2>PIONEER BOOKSHOP MANAGERS</h2>
+        <button>Home</button>
       </div>
-      <div className="login-footer">
-        <div className="register">
-          <p>
-            Have an account?
-            <span>
-              <Link
-                to={myRouteLocation !== "/manager/signup" ? "/login" : "/manager/login"}
-              >
-                Login
-              </Link>
-            </span>
-          </p>
+      <div className="registration-container">
+        <div className="registration-signup-form">
+          <h3>Join Us</h3>
+          <form onSubmit={handleSubmit} autoComplete="off">
+            <div className="registration-inputs-container">
+              <input
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Name"
+                // value={formState.name}
+                onChange={(e) => formChange(e)}
+                required
+              />
+              <br/>
+              <input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Email address"
+                // value={formState.email}
+                onChange={(e) => formChange(e)}
+                required
+              />
+                            <br/>
+              <input
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Password"
+                // value={formState.password}
+                onChange={(e) => formChange(e)}
+                required
+              />
+                            <br/>
+              <input
+                id="passwordConfirm"
+                type="password"
+                name="password_confirmation"
+                placeholder="Confirm password"
+                // value={passConfirmation.passwordConfirm}
+                onChange={(e) => formChange(e)}
+                required
+              />
+            </div>
+            <br/>
+            <div className="registration-button-form">
+              <button onClick={() => navigate("/login")}>Continue</button>
+            </div>
+          </form>
+        </div>
+        <div className="registration-footer">
+          {/* <div className="register">
+            <p>
+              Have an account?
+              <span>
+                <Link
+                  to={
+                    myRouteLocation !== "/manager/signup"
+                      ? "/login"
+                      : "/manager/login"
+                  }
+                >
+                  Login
+                </Link>
+              </span>
+            </p>
+          </div> */}
+        </div>
+        <div className="registration-welcome-message">
+          <img  src={logo} alt="#"/>
+          <h5>Welcome aboard my friend</h5>
+          <p>just a couple of clicks and we start</p>
         </div>
       </div>
     </div>
