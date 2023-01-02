d598f7c7c4d89129bc8a1e0e845e16c13235b9da
moringawonder <adams.wonderboy@student.moringaschool.com>
2023-01-02 11:52:18 +0300

    BMA-18 adds subtotal to the items table

--- client/src/Components/Employee/MakeASale/Subtotal.js
+++ client/src/Components/Employee/MakeASale/Subtotal.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 // TODO: create subtotal states and return data from makeASale componennt
 const Subtotal = () => {
-  return (
-    <div>Subtotal</div>
-  )
+    return (
+        <>
+            <div>Subtotal</div>
+
+            {/* Should display the subtotal to the right bottom of the items table when rendered */}
+            <div className='row'>
+                <div className='col-md-8'></div>
+                <div className='col-md-4'>
+                    <div className='card card-body mt-3'>
+                        <h4>Sub Total:
+                            {/* Gets the total item price into the cart */}
+                            <span className='float-end'>{totalCartPrice}</span>
+                        </h4>
+                        <h4>Grand Total
+                            {/* Matches the subtotal if no discount or change is made */}
+                            <span className='float-end'>{totalCartPrice}</span>
+                        </h4>
+                        <hr />
+                        {/* Navigates to checkout page for a successfull sale */}
+                        <Link to="/checkout" className='btn btn-primary'>checkout</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Subtotal
+export default Subtotal 
