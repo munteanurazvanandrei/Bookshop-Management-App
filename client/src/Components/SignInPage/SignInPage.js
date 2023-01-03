db5cc5558b46f7320d5ab54f47f7545a5b575b32
Gibson-Bett <gibubett@gmail.com>
2023-01-03 13:16:12 +0300

    BMA-16# setup signin  framework to maintain the signup classnames for easy styling

--- client/src/Components/SignInPage/SignInPage.js
+++ client/src/Components/SignInPage/SignInPage.js
@@ -1,4 +1,49 @@
-export default function SiginIn(){
+export default function SignIn(){
     // The signIn page
-    return (<div className="sign-in"></div>)
+
+
+    return (<div className="sign-in">
+        <div className="registration">
+      <div className="registration-nav">
+        <h2>PIONEER BOOKSHOP MANAGERS</h2>
+        <button>Home</button>
+      </div>
+      <div className="registration-container">
+        <div className="registration-signup-form">
+          <h3>Welcome back</h3>
+          <form  autoComplete="off">
+            <div className="registration-inputs-container">
+              <input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Email address"
+                // value={formState.email}
+                required
+              />
+                            <br/>
+              <input
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Password"
+                // value={formState.password}
+                required
+              />        
+            </div>
+            <br/>
+            <div className="registration-button-form">
+              <button>Login</button>
+              <button>Register</button>
+            </div>
+          </form>
+        </div>
+        <div className="registration-welcome-message">
+          {/* <img  src={logo} alt="#"/> */}
+          <h5>Hey, welcome back</h5>
+          <p>Good to see you again</p>
+        </div>
+      </div>
+    </div>
+    </div>)
 }
