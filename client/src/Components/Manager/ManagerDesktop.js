855a02c33e1537491e538fdf2896b5220d9cc51e
James Mburu Githire <jamesmburug24@gmail.com>
2023-01-03 09:46:14 +0300

    development - added Employee sales to managerDsktp

--- client/src/Components/Manager/ManagerDesktop.js
+++ client/src/Components/Manager/ManagerDesktop.js
@@ -3,6 +3,7 @@ import AddOrEditItem from "./AddOrEdit/AddOrEditItem";
 import Employees from "./Employees/Employees";
 import Items from "./Items/Items";
 import ManagerSideBar from "./ManagerSidebar/ManagerSidebar";
+import EmployeeSales from "./EmployeeSales/EmployeeSales";
 import { Route, Routes } from "react-router-dom";
 import "./managerDesktop.css";
 
@@ -12,6 +13,7 @@ export default function ManagerDesktop(){
     <ManagerSideBar managerName={"John"}/>
         <Routes>
             <Route path="/employees" element={<Employees/>}/>
+            <Route path="/employees_sales" element={<EmployeeSales/>}/>
             <Route path="/employees" element={<Items/>}/>
             <Route path="/add_or_edit_employee" element={<AddOrEditEmployee/>}/>
             <Route path="/add_or_edit_item" element={<AddOrEditItem/>}/>
