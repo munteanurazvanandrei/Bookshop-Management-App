8566f60fb84d0b571b367be8658fc737a30b996d
Gibson-Bett <gibubett@gmail.com>
2023-01-03 14:52:54 +0300

    BMA-16# fetch managers details on form submit.

--- client/src/Components/SignInPage/SignInPage.js
+++ client/src/Components/SignInPage/SignInPage.js
@@ -1,60 +1,86 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 
-export default function SignIn(){
-    // The signIn page
-    const [formData, setFormData] = useState({})
-    const [errors, setErrors] = useState("")
+export default function SignIn(handleLogin, action = "") {
+  // The signIn page
+  const [formData, setFormData] = useState({});
+  const [errors, setErrors] = useState("");
 
-     function handleInput(e){
-        const key = e.target.name
-        const value = e.target.value
-        
-        setErrors("")
-        setFormData({...formData, [key]: value})
-     }
+  function handleInput(e) {
+    const key = e.target.name;
+    const value = e.target.value;
 
-    return (<div className="sign-in">
-        <div className="registration">
-      <div className="registration-nav">
-        <h2>PIONEER BOOKSHOP MANAGERS</h2>
-        <button>Home</button>
-      </div>
-      <div className="registration-container">
-        <div className="registration-signup-form">
-          <h3>Welcome back</h3>
-          <form  autoComplete="off">
-            <div className="registration-inputs-container">
-              <input
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Email address"
-                // value={formState.email}
-                required
-              />
-                            <br/>
-              <input
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Password"
-                // value={formState.password}
-                required
-              />        
-            </div>
-            <br/>
-            <div className="registration-button-form">
-              <button>Login</button>
-              <button>Register</button>
-            </div>
-          </form>
+    setErrors("");
+    setFormData({ ...formData, [key]: value });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    action
+      ? fetch("/managers", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }).then((r) => {
+          if (r.ok) {
+            return r.json().then((user) => {
+              // console.log(user)
+              handleLogin.handleManagerLogin(user);
+            });
+          }
+        })
+      : fetch();
+  }
+
+  return (
+    <div className="sign-in">
+      <div className="registration">
+        <div className="registration-nav">
+          <h2>PIONEER BOOKSHOP MANAGERS</h2>
+          <button>Home</button>
         </div>
-        <div className="registration-welcome-message">
-          {/* <img  src={logo} alt="#"/> */}
-          <h5>Hey, welcome back</h5>
-          <p>Good to see you again</p>
+        <div className="registration-container">
+          <div className="registration-signup-form">
+            <h3>Welcome back</h3>
+            <form onSubmit={handleSubmit} autoComplete="off">
+              <div className="registration-inputs-container">
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Email address"
+                  required
+                  onChange={(e) => {
+                    handleInput(e);
+                  }}
+                />
+                <br />
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                  onChange={(e) => {
+                    handleInput(e);
+                  }}
+                />
+              </div>
+              <br />
+              <div className="registration-button-form">
+                <button>Login</button>
+                <span>Register</span>
+              </div>
+            </form>
+          </div>
+          <div className="registration-welcome-message">
+            {/* <img  src={logo} alt="#"/> */}
+            <h5>Hey, welcome back</h5>
+            <p>Good to see you again</p>
+          </div>
         </div>
       </div>
     </div>
-    </div>)
+  );
 }
