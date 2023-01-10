acf9a4537853b5fdd8b54031537624d19fca1fbc
Gibson-Bett <gibubett@gmail.com>
2023-01-11 00:16:03 +0300

    BMA-16# add icons and style

--- client/src/Components/LandingPage/LandingPage.js
+++ client/src/Components/LandingPage/LandingPage.js
@@ -1,7 +1,10 @@
 import React from "react";
-import "./landingPage.css"
+import "./landingPage.css";
 import { useNavigate } from "react-router-dom";
-
+import {
+  HiOutlineLogout,
+  HiOutlineBookmarkAlt,
+} from "react-icons/hi";
 
 export default function LandingPage() {
   // Landing page
@@ -11,20 +14,30 @@ export default function LandingPage() {
     <div className="landing-page">
       <div className="landing-page-nav">
         <h2>PIONEER BOOKSHOP MANAGERS</h2>
-            <div className="landing-button">
-            <button onClick={() => navigate("/signin")}>Login</button>
-            <button>Discover</button>
-            </div>
+        <div className="landing-button">
+          <div>
+            <button onClick={() => navigate("/signin")} className='home_link'>Login</button>
+              <HiOutlineLogout className="landing_page_icon"/>
+          </div>
+          <div>
+            <button className="home_link">Discover</button>
+              <HiOutlineBookmarkAlt className="landing_page_icon"/>
+          </div>
+        </div>
       </div>
       <div className="landing-page-message">
         <h1>The companion app to manage your bookshop</h1>
         <img src="/svgs/landing-books.svg" alt="Books logo"></img>
       </div>
       <div className="landing-page-message">
-        <h5>Make sales , Track orders, track cash in any device at any time.</h5>
+        <h5>
+          Make sales , Track orders, track cash in any device at any time.
+        </h5>
       </div>
       <div className="landing-button">
-        <button id="landing-join-us-button" onClick={() => navigate("/signup")}>Join Us</button>
+        <button id="landing-join-us-button" onClick={() => navigate("/signup")}>
+          Join Us
+        </button>
       </div>
     </div>
   );
