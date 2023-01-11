dddba2c529bd7473b513055c8b152808c826c4e9
James Mburu Githire <jamesmburug24@gmail.com>
2023-01-11 18:56:15 +0300

    development removed bang methods

--- app/controllers/jwt_controller.rb
+++ app/controllers/jwt_controller.rb
@@ -6,8 +6,8 @@ class JwtController < ApplicationController
     def create
         # byebug
         user = manager_access? ? 
-            Manager.find_by!(email: params[:email]) : 
-            Employee.find_by!(email: params[:email])
+            Manager.find_by(email: params[:email]) : 
+            Employee.find_by(email: params[:email])
         # checks if the user is valid and then proceeds to encode
         if user&.authenticate(params[:password])
             token = encode_token(user_id: user.id)
