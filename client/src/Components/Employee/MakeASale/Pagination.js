e38ffaca99b8f1c719b8bb587ca9bc3c98f6e82c
moringawonder <adams.wonderboy@student.moringaschool.com>
2023-01-13 14:01:55 +0300

    BMA-18 adds Pagination component

--- client/src/Components/Employee/MakeASale/Pagination.js
+++ client/src/Components/Employee/MakeASale/Pagination.js
@@ -1,29 +1,43 @@
-import React, { useState } from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../MakeASale/styling/pagination.css';
 
-const Pagination = () => {
-    // User is currently on this page
-    const [currentPage, setCurrentPage] = useState(1);
-    // Number of Records to be displayed on each page   
-    const [recordsPerPage] = useState(10);
+const Pagination = ({ productsPerPage, filteredItems, paginate, currentPage }) => {
+    const pageNumbers = [];
 
-    const indexOfLastRecord = currentPage * recordsPerPage;
-    const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+    for (let i = 1; i <= Math.ceil(filteredItems.length / productsPerPage); i++) {
+        pageNumbers.push(i);
+    }
 
-    // Records to be displayed on the current page
-    const currentRecords = filteredItems.slice(indexOfFirstRecord, indexOfLastRecord);
-
-    // Calculating the number of pages
-    const nPages = Math.ceil(filteredItems.length / recordsPerPage)
-
-    // Array holding page numbers
-    const pageNumbers = [...Array(nPages + 1).keys()].slice(1)
+    function renderFirstPage() {
+        return pageNumbers[0];
+    }
+    function renderLastPage() {
+        return pageNumbers[pageNumbers.length - 1];
+    }
 
     return (
-        <>
-            <div>Pagination</div>
-            
-        </>
-    )
+        <nav className='pagination-container'>
+            <NavLink to='#' onClick={() => paginate(renderFirstPage())}>
+                «
+            </NavLink>
+            {pageNumbers.map((number) => {
+                return (
+                    <NavLink
+                        to='#'
+                        key={number}
+                        onClick={() => paginate(number)}
+                        id={currentPage === number ? 'active-page' : ''}
+                    >
+                        {number}
+                    </NavLink>
+                );
+            })}
+            <NavLink to='#' onClick={() => paginate(renderLastPage())}>
+                »
+            </NavLink>
+        </nav>
+    );
 }
 
-export default Pagination
+export default Pagination;
