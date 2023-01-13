e5d9b984d95b3d861682dbe2987b314dd247ffb6
James Mburu Githire <jamesmburug24@gmail.com>
2023-01-13 19:01:31 +0300

    BMA-22 added Authorization

--- client/src/Components/Manager/EmployeeSales/EmployeeSales.js
+++ client/src/Components/Manager/EmployeeSales/EmployeeSales.js
@@ -1,5 +1,5 @@
-import "./EmployeeSales.css";
-import React, { useState, useEffect } from "react";
+import './EmployeeSales.css'
+import React, { useState, useEffect } from 'react'
 
 export default function EmployeeSales() {
   // Display all transactions
@@ -9,36 +9,47 @@ export default function EmployeeSales() {
   // Table columns:
   // Table rows > contain the sale details
 
-  const [employeeSales, setemployeeSales] = useState([]);
+  const [employeeSales, setemployeeSales] = useState()
   useEffect(() => {
-    fetch("http://localhost:3000/sales_transactions")
+    fetch('http://localhost:3000/sales_transactions',{
+      headers:{"Authorization":`Bearer ${localStorage.getItem("jwt")}`,
+            "role":"manager"
+    }
+    })
       .then((response) => response.json())
       .then((data) => {
         console.log(data)
-        setemployeeSales(data);});
-	}, []);
+        setemployeeSales(data)
+      })
+  }, [])
 
   // return <div>{employeeSales.map((sales)=>sales.employee_name)}</div>;
   return (
     <div className="employee-sales">
-      <h1>Bookshop manager</h1>
+      <h1>Bookshop Manager</h1>
 
-       <table>
-		<tr>
-		<th>Date</th>
-		<th>employee</th>
-		<th>Total</th>
-		<th>Recieved</th>
-		<th>Change</th>
-		</tr>
-		{employeeSales.map(sale=><tr>
-			<td>{sale.created_at}</td>
-			<td>{sale.employee_name}</td>
-			<td>{sale.amount}</td>
-			<td>{sale.recieved}</td>
-			<td>{sale.change}</td>
-			</tr>)}
-	</table>
-	</div>
-		);
+      <table>
+        <thead>
+        <tr>
+          <th>Date</th>
+          <th>Employee</th>
+          <th>Total</th>
+          <th>Recieved</th>
+          <th>Change</th>
+        </tr>
+        </thead>
+        <tbody>
+          {employeeSales && employeeSales.map((sale,ind) => (
+            <tr key={ind}>
+              <td>{sale.created_at.replace("T"," - ").replace("Z","")}</td>
+              <td>{sale.employee_name}</td>
+              <td>{sale.amount}</td>
+              <td>{sale.recieved}</td>
+              <td>{sale.change}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
