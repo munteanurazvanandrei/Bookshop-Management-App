b2284f20512d471719f7cf313550e0ebe469b2bf
moringawonder <adams.wonderboy@student.moringaschool.com>
2023-01-16 14:34:25 +0300

    BMA-18 adds search

--- client/src/Components/Employee/MakeASale/Search.js
+++ client/src/Components/Employee/MakeASale/Search.js
@@ -1,18 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-const Search = ({handleChange}) => {
-    
-    // initialize state for user input during search
-  
-
-    // checks if search input field recieves any character and filters to match name of entity entered
-
-    return (
-    <div className="search">
-        {/* Returns search as an input type */}
-        <input type="search" placeholder='Enter To Start Search' onChange={handleChange} />
-    </div>
+const Search = () => {
+  return (
+    <div>Search</div>
   )
 }
 
-export default Search;
+export default Search
