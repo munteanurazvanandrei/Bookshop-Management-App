f425a49aac98df0b0cae83db03af9ef0f0b2aa49
James Mburu Githire <jamesmburug24@gmail.com>
2023-01-18 16:10:32 +0300

    removed transaction validation

--- app/models/sales_transaction.rb
+++ app/models/sales_transaction.rb
@@ -1,6 +1,5 @@
 class SalesTransaction < ApplicationRecord
     has_many :sales
     belongs_to :employee
-    delegate :manager, to: :employee 
-    validates :employee_id, presence: true
+    delegate :manager, to: :employee
 end
